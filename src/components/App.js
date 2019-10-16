941451e7d12cafbb741ebd64038ba16da1afce06
Zbigniew Baranski <zb.baranski@icloud.com>
2019-10-16 21:33:37 +0200

    basic links done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
+import Nav from './Nav';
+import About from './About';
+import Shop from './Shop';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+// Route renders all matching components i.e. when path='/' and path='about' it will render both
+// Switch makes sure after first match there's no more components rendered
+// to render Home page only use the 'exact' keyword!
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Router>
+      <div className='App'>
+        <Nav />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/shop' component={Shop} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
 
 export default App;
